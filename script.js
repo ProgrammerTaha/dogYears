3b2a947f4c4ba0543f6741c94cbc5414e3c4be93
programmertaha <programmer.taha.1y@gmail.com>
2020-08-31 06:16:20 +0300

    #1 task

---
+++ script.js
@@ -0,0 +1,10 @@
+/*
+time moves quickly for dogs
+8 years in a human’s life equates to 45 years in a dog’s life
+first two years of a dog’s life count as 10.5 dog years each
+Each year following equates to 4 dog years
+*/
+
+//My age
+let myAge = 24;
+
